7e855656496e58225a4ed19c19d431e1ad6dcdbd
402680394 <402680394@qq.com>
2018-06-08 17:28:13 +0800

    调整bug

--- src/main/java/com/ztdx/eams/Application.java
+++ src/main/java/com/ztdx/eams/Application.java
@@ -1,12 +1,9 @@
 package com.ztdx.eams;
 
-import com.ztdx.eams.basic.utils.FileReaderUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import java.io.File;
 
 @SpringBootApplication
 @EnableJpaRepositories
