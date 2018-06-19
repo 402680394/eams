7dda32602320336c0d9955585b437098f7606b6a
sarah <sarah@ztdx.com>
2018-06-19 11:32:32 +0800

    修改库房列表

--- src/main/java/com/ztdx/eams/domain/archives/model/condition/EntryCondition.java
+++ src/main/java/com/ztdx/eams/domain/archives/model/condition/EntryCondition.java
@@ -1,8 +1,10 @@
 package com.ztdx.eams.domain.archives.model.condition;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ztdx.eams.domain.archives.model.DescriptionItemDataType;
 import lombok.Data;
 
+import javax.persistence.Convert;
 import java.util.List;
 
 @Data
@@ -18,6 +20,7 @@ public class EntryCondition {
     /**
      * 条件的类型，用于区分系统和自定义。不需要前端传
      */
+    @Convert(converter = EntryConditionType.class)
     private EntryConditionType entryConditionType;
 
     /**
