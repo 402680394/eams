937ab4ed661d43c2a6122cd5c15c56d5456a659b
jet_li <jet_li@sinobasedm.com>
2018-08-14 15:29:11 +0800

    待办接口签入

--- src/main/java/com/ztdx/eams/controller/work/BacklogController.java
+++ src/main/java/com/ztdx/eams/controller/work/BacklogController.java
@@ -1,4 +1,4 @@
-package com.ztdx.eams.work;
+package com.ztdx.eams.controller.work;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,12 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class BacklogController {
 
     /**
-     * @api {get} /backlog/todoList?page={page}&size={size} 待办列表
-     * @apiName list
+     * @api {get} /backlog/todoList?page={page}&size={size} 我的待办列表
+     * @apiName todoList
      * @apiGroup backlog
      * @apiParam {Number} [page] 页码(QueryString)
      * @apiParam {Number} [size] 页行数(QueryString)
-     * @apiSuccess {String="loan"} type 待办类型 loan:借阅
+     * @apiSuccess {String="borrow"} type 待办类型 borrow:借阅
      * @apiSuccess {String} title 标题
      * @apiSuccess {String} orderNo 单号
      * @apiSuccess {Object} applicant 申请人
@@ -23,7 +23,7 @@ public class BacklogController {
      * @apiSuccess {Number} createTime 申请时间
      * @apiSuccessExample {json} Response-Example:
      * {
-     *     "type": "loan",
+     *     "type": "borrow",
      *     "title": "论持久战",
      *     "orderNo": "xxxxx-xxxx-xxx",
      *     "applicant": {
@@ -34,7 +34,33 @@ public class BacklogController {
      *     "createTime":1534214071
      * }
      */
-    public void top10(){
+    public void todoList(){
+
+    }
+
+    /**
+     * @api {get} /backlog/applyList?page={page}&size={size} 我的申请列表
+     * @apiName applyList
+     * @apiGroup backlog
+     * @apiParam {Number} [page] 页码(QueryString)
+     * @apiParam {Number} [size] 页行数(QueryString)
+     * @apiSuccess {String="borrow"} type 待办类型 borrow:借阅
+     * @apiSuccess {String} title 标题
+     * @apiSuccess {String} orderNo 单号
+     * @apiSuccess {Number} createTime 申请时间
+     * @apiSuccess {Number} endTime 审批时间
+     * @apiSuccess {String="pending", "refuse", "agree"} result 结果
+     * @apiSuccessExample {json} Response-Example:
+     * {
+     *     "type": "borrow",
+     *     "title": "论持久战",
+     *     "orderNo": "xxxxx-xxxx-xxx",
+     *     "createTime":1534214071,
+     *     "endTime": null,
+     *     "result": "pending",
+     * }
+     */
+    public void applyList(){
 
     }
 
@@ -42,7 +68,7 @@ public class BacklogController {
      * @api {get} /backlog/list?page={page}&size={size} 工作列表
      * @apiName list
      * @apiGroup backlog
-     * @apiSuccess {String="loan"} type 待办类型 loan:借阅
+     * @apiSuccess {String="borrow"} type 待办类型 borrow:借阅
      * @apiSuccess {String} title 标题
      * @apiSuccess {String} orderNo 单号
      * @apiSuccess {Object} applicant 申请人
@@ -51,10 +77,10 @@ public class BacklogController {
      * @apiSuccess {String} applicant.department 部门
      * @apiSuccess {Number} createTime 申请时间
      * @apiSuccess {Number} endTime 审批时间
-     * @apiSuccess {String="pending", "refuse", "agree"} result 状态
+     * @apiSuccess {String="pending", "refuse", "agree"} result 结果
      * @apiSuccessExample {json} Response-Example:
      * {
-     *     "type": "loan",
+     *     "type": "borrow",
      *     "title": "论持久战",
      *     "orderNo": "xxxxx-xxxx-xxx",
      *     "applicant": {
@@ -62,7 +88,9 @@ public class BacklogController {
      *         "company": "",
      *         "department": ""
      *     },
-     *     "createTime":1534214071
+     *     "createTime":1534214071,
+     *     "endTime": 1534214071,
+     *     "result": "agree",
      * }
      */
     public void list(){
